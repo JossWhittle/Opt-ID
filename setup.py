25f609328496568987ee340bc2abb26c02560599
Joss Whittle <joss.whittle@gmail.com>
2020-08-12 15:36:17 +0100

    Initial commit for r3.0 development branch. Starts with a fresh project folder hierarchy and a (hopefully) correct travis/coveralls/scrutinizer configuration.

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 from setuptools import setup, find_packages
 
-
 setup(
+    version='3.0a',
     name='opt-id',
     description='Optimisation of IDs using Python and Opt-AI',
     url='https://github.com/DiamondLightSource/Opt-ID',
-    author='Mark Basham',
-    author_email='mark.basham@diamond.ac.uk',
-    packages=find_packages(),
+    author='Mark Basham, Joss Whittle',
+    author_email='mark.basham@rfi.ac.uk, joss.whittle@rfi.ac.uk',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    test_suite='tests',
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 2 - Pre-Alpha',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Operating System :: POSIX :: Linux',
     ],
     license='Apache License, Version 2.0',
